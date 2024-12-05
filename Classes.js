5f7832c0db6ce30a43eb7d7370d2eb3f5587de1c
y2lucas <lucasvasconcelos6909@gmail.com>
2024-12-05 15:43:19 -0300

    asadasd

---
+++ Classes.js
@@ -0,0 +1,178 @@
+window.onload = function () {
+    var img = document.getElementById('img');
+    var img1 = document.getElementById('img1');
+    var img2 = document.getElementById('img2');
+    var img3 = document.getElementById('img3');
+    var img4 = document.getElementById('img4');
+    var img5 = document.getElementById('img5');
+    var img6 = document.getElementById('img6');
+    var img7 = document.getElementById('img7');
+    var img8 = document.getElementById('img8');
+    var img9 = document.getElementById('img9');
+    var img10 = document.getElementById('img10');
+    var img11 = document.getElementById('img11');
+    var img12 = document.getElementById('img12');
+    var img13 = document.getElementById('img13');
+    var img14 = document.getElementById('img14');
+    var img15 = document.getElementById('img15');
+    var img16 = document.getElementById('img16');
+    var img17 = document.getElementById('img17');
+    var img18 = document.getElementById('img18');
+    var img19 = document.getElementById('img19');
+    var img20 = document.getElementById('img20');
+    var img21 = document.getElementById('img21');
+
+    var fundo = document.getElementById('FUNDO');
+    var fundo1 = document.getElementById('FUNDO1');
+    var fundo2 = document.getElementById('FUNDO2');
+    var fundo3 = document.getElementById('FUNDO3');
+    var fundo4 = document.getElementById('FUNDO4');
+    var fundo5 = document.getElementById('FUNDO5');
+    var fundo6 = document.getElementById('FUNDO6');
+    var fundo7 = document.getElementById('FUNDO7');
+    var fundo8 = document.getElementById('FUNDO8');
+
+    setTimeout(function () {
+        fundo.style.transform = 'translateY(0)';
+        fundo.style.opacity = 1;
+    }, 450);
+
+    setTimeout(function () {
+        fundo1.style.transform = 'translateX(0)';
+        fundo1.style.opacity = 1;
+    }, 330);
+
+    setTimeout(function () {
+        fundo2.style.transform = 'translateX(0)';
+        fundo2.style.opacity = 1;
+    }, 400);
+    setTimeout(function () {
+        fundo3.style.transform = 'translateX(0)';
+        fundo3.style.opacity = 1;
+    }, );
+    setTimeout(function () {
+        fundo4.style.transform = 'translateX(0)';
+        fundo4.style.opacity = 1;
+    }, );
+    setTimeout(function () {
+        fundo5.style.transform = 'translateX(0)';
+        fundo5.style.opacity = 1;
+    }, );
+    setTimeout(function () {
+        fundo6.style.transform = 'translateX(0)';
+        fundo6.style.opacity = 1;
+    }, );
+    setTimeout(function () {
+        fundo7.style.transform = 'translateX(0)';
+        fundo7.style.opacity = 1;
+    }, );
+    setTimeout(function () {
+        fundo8.style.transform = 'translateX(0)';
+        fundo8.style.opacity = 1;
+    }, );
+
+    setTimeout(function () {
+        img.style.transform = 'translateX(0)';
+        img.style.opacity = 1;
+    }, );
+
+    setTimeout(function () {
+        img1.style.transform = 'translateX(0)';
+        img1.style.opacity = 1;
+    }, );
+
+    setTimeout(function () {
+        img2.style.transform = 'translateX(0)';
+        img2.style.opacity = 1;
+    }, );
+
+    setTimeout(function () {
+        img3.style.transform = 'translateX(0)';
+        img3.style.opacity = 1;
+    }, );
+
+    setTimeout(function () {
+        img4.style.transform = 'translateX(0)';
+        img4.style.opacity = 1;
+    }, );
+
+    setTimeout(function () {
+        img5.style.transform = 'translateX(0)';
+        img5.style.opacity = 1;
+    }, );
+
+    setTimeout(function () {
+        img6.style.transform = 'translateX(0)';
+        img6.style.opacity = 1;
+    }, );
+    setTimeout(function () {
+        img7.style.transform = 'translateX(0)';
+        img7.style.opacity = 1;
+    }, );
+    setTimeout(function () {
+        img8.style.transform = 'translateX(0)';
+        img8.style.opacity = 1;
+    }, );
+    setTimeout(function () {
+        img9.style.transform = 'translateX(0)';
+        img9.style.opacity = 1;
+    }, );
+    setTimeout(function () {
+        img10.style.transform = 'translateX(0)';
+        img10.style.opacity = 1;
+    }, );
+    setTimeout(function () {
+        img11.style.transform = 'translateX(0)';
+        img11.style.opacity = 1;
+    }, );
+    setTimeout(function () {
+        img12.style.transform = 'translateX(0)';
+        img12.style.opacity = 1;
+    }, );
+    setTimeout(function () {
+        img13.style.transform = 'translateX(0)';
+        img13.style.opacity = 1;
+    }, );
+    setTimeout(function () {
+        img14.style.transform = 'translateX(0)';
+        img14.style.opacity = 1;
+    }, );
+    setTimeout(function () {
+        img15.style.transform = 'translateX(0)';
+        img15.style.opacity = 1;
+    }, );
+    setTimeout(function () {
+        img16.style.transform = 'translateX(0)';
+        img16.style.opacity = 1;
+    }, );
+    setTimeout(function () {
+        img17.style.transform = 'translateX(0)';
+        img17.style.opacity = 1;
+    }, );
+    setTimeout(function () {
+        img18.style.transform = 'translateX(0)';
+        img18.style.opacity = 1;
+    }, );
+    setTimeout(function () {
+        img19.style.transform = 'translateX(0)';
+        img19.style.opacity = 1;
+    }, );
+    setTimeout(function () {
+        img20.style.transform = 'translateX(0)';
+        img20.style.opacity = 1;
+    }, );
+    setTimeout(function () {
+        img21.style.transform = 'translateX(0)';
+        img21.style.opacity = 1;
+    }, );
+
+
+
+
+
+
+
+
+
+
+}
